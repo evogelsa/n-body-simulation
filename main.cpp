8dbaeb32e4b480c39db4ef308da6d572371ef1a2
Ethan Vogelsang <ethanvogelsang@gmail.com>
2021-11-15 17:29:44 -0600

    add satellite results

--- main.cpp
+++ main.cpp
@@ -10,15 +10,39 @@
 
 int main() {
   const double time_step = 0.1;
-  const int max_time = 5000;
-  const int max_steps = max_time / time_step;
-  const int points_per_body = 1000;
+  const int max_time = 365*24*60*60;
+  const unsigned long long max_steps = max_time / time_step;
+  const int points_per_body = 10000;
   const int log_resolution = max_steps / points_per_body;
 
   std::vector<Body> bodies;
-  bodies.push_back(Body(Vector3D(0, 0, 0), Vector3D(0, 0, -1e-2), 1e6));
-  bodies.push_back(Body(Vector3D(0, 1, 0), Vector3D(1e-2, 1e-3, 0), 1e6));
-  bodies.push_back(Body(Vector3D(0, 0, 1), Vector3D(-1e-3, 0, 1e-3), 1e5));
+
+  // sun
+  bodies.push_back(Body(
+        Vector3D(0, 0, 0),
+        Vector3D(0, 0, 0),
+        1.988435e30
+  ));
+
+  // earth
+  const double r_e = 1.49597887e11;
+  const double v_e = 29800;
+  const double m_e = 5.97e24;
+  bodies.push_back(Body(
+        Vector3D(r_e, 0, 0),
+        Vector3D(0, v_e, 0),
+        m_e
+  ));
+
+  // satellite
+  const double r_s = r_e + 7378140;
+  const double v_s = v_e + 7350;
+  const double m_s = 1e3;
+  bodies.push_back(Body(
+        Vector3D(r_s, 0, 0),
+        Vector3D(0, v_s, 0),
+        m_s
+  ));
 
   // std::default_random_engine generator;
   // std::uniform_real_distribution<double> distribution(-1.0, 1.0);
--- plot.py
+++ plot.py
@@ -4,7 +4,12 @@ import sys
 
 def main():
     # read data
-    with open('output.csv', 'r') as f:
+    fn = 'output.csv'
+    for arg in sys.argv:
+        if '.csv' in arg:
+            fn = arg
+
+    with open(fn, 'r') as f:
         lines = f.readlines()
 
     # create 3d axes figure
@@ -29,9 +34,9 @@ def main():
         ax.scatter(bodies[body]['x'], bodies[body]['y'], bodies[body]['z'], 'o')
 
 
-    ax.set_xlim3d(-3, 10)
-    ax.set_ylim3d(-3, 10)
-    ax.set_zlim3d(-10, 3)
+    #  ax.set_xlim3d(-3, 10)
+    #  ax.set_ylim3d(-3, 10)
+    #  ax.set_zlim3d(-10, 3)
 
     ax.set_xlabel('x axis')
     ax.set_ylabel('y axis')
