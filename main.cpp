26e7145cf2648d31966d316120fccde096ad6cb0
Ethan Vogelsang <ethanvogelsang@gmail.com>
2021-12-13 23:04:33 -0600

    add more results

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <random>
 #include <sys/time.h>
 #include <vector>
+#include <omp.h>
 
 #include "body.h"
 #include "integration.h"
@@ -12,32 +13,48 @@
 
 int main() {
   // simulation parameters
-  const double time_step = 1;
-  const int max_time = 1000;
+  const double time_step = 6;
+  const int max_time = 2*28*24*60*60;
   const unsigned long long max_steps = max_time / time_step;
   const int points_per_body = 1000;
   const int log_resolution = max_steps / points_per_body;
 
-  // vector to hold each body
   std::vector<Body> bodies;
 
-  // randomly generate initial conditions for each body
-  std::default_random_engine generator;
-  std::uniform_real_distribution<double> distribution(-1.0, 1.0);
+  // earth
+  const double r_e = 0;
+  const double v_e = 0;
+  printf("v_e = %f\n", v_e);
+  const double m_e = 5.97e24;
+  bodies.push_back(Body(
+        Vector3D(r_e, 0, 0),
+        Vector3D(0, v_e, 0),
+        m_e
+  ));
 
-  // create each body with initial conditions and add to vector
-  int nbodies = 1000;
-  for (int i = 0; i < nbodies; i++) {
-    double x = distribution(generator) * 2;
-    double y = distribution(generator) * 2;
-    double z = distribution(generator) * 2;
-    double vx = distribution(generator) * 1e-3;
-    double vy = distribution(generator) * 1e-3;
-    double vz = distribution(generator) * 1e-3;
-    double m = 1e5;
+  // moon
+  const double r_m = 3.844e8;
+  const double v_m = 1020;
+  const double m_m = 7.3459e22;
+  bodies.push_back(Body(
+        Vector3D(r_m + r_e, 0, 0),
+        Vector3D(0, v_m, 0),
+        m_m
+  ));
 
-    bodies.push_back(Body(Vector3D(x, y, z), Vector3D(vx, vy, vz), m));
-  }
+  // // L4 lagrange point
+  // const double l4_x = r_m / 2.0;
+  // const double l4_y = r_m * std::sqrt(3) / 2;
+  //
+  // // satellite
+  // const double m_s = 1e3;
+  // const double v_x = -v_m * std::cos(M_PI / 6);
+  // const double v_y = v_m * std::sin(M_PI / 6);
+  // bodies.push_back(Body(
+  //     Vector3D(l4_x, l4_y, 0),
+  //     Vector3D(v_x, v_y, 0),
+  //     m_s
+  // ));
 
   // create an output file
   std::ofstream output("output.csv", std::ios::out);
@@ -53,13 +70,20 @@ int main() {
 
   // simulate for determined number of steps
   for (int i = 0; i < max_steps; i++) {
+    double x, y, z;
+    x = simulation.bodies[0].velocity.x;
+    y = simulation.bodies[0].velocity.y;
+    z = simulation.bodies[0].velocity.z;
     // print progress
-    printf("\r%d / %llu (%0.1f%%)", i + 1, max_steps,
-           (float)(i + 1) / max_steps * 100);
+    printf("\r%d / %llu (%0.1f%%) | Vel(%7.3f, %7.3f, %7.3f)", i + 1, max_steps,
+           (float)(i + 1) / max_steps * 100, x, y, z);
     std::fflush(stdout);
 
     // compute next simulation step
     simulation.gravity_step();
+
+    // simulation.bodies[0].position = Vector3D(0, 0, 0);
+    // simulation.bodies[0].velocity = Vector3D(0, 0, 0);
 
     // log at the desired resolution (snapshot of sim taken every x steps)
     if (i % log_resolution == 0)
